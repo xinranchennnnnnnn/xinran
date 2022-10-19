8057edf2ecfdad1de8818205198c082e515b6966
xinranchennnnnnnn <112905252+xinranchennnnnnnn@users.noreply.github.com>
2022-10-18 21:49:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let app = express();
+
+app.get('/',(request, response)=> {
+    response.send("hello");
+})
+
+app.get('/about',(request,response) => {
+    response.send("this is an about page");
+})
+
+app.listen(3000,()=> {
+    console.log("app is listening at localhost:3000");
+})
+
